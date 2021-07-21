c2424368ed6e047c4d5286aadc0a856fdb695834
Hyeseong Kim <tim@daangn.com>
2021-07-22 02:45:51 +0900

    chore: add official msgpack impl to the benchmark

--- benchmarks/msgpack-complex.js
+++ benchmarks/msgpack-complex.js
@@ -1,12 +1,18 @@
 const { Benchmark } = require('benchmark');
 
 const urlpack = require('@urlpack/msgpack/lib');
+const msgpack = require('@msgpack/msgpack');
 const msgpack5 = require('msgpack5')();
 const msgpackLite = require('msgpack-lite');
 
+const date = new Date(1626881119799);
+
 new Benchmark.Suite()
-.add('encode complex 1 - urlpack', () => {
+.add('encode complex 1 - @urlpack/msgpack', () => {
   urlpack.encode({ a: { b: 'c' }, c: [null, null, null, { d: 'c' }] });
+})
+.add('encode complex 1 - @msgpack/msgpack', () => {
+  msgpack.encode({ a: { b: 'c' }, c: [null, null, null, { d: 'c' }] });
 })
 .add('encode complex 1 - msgpack5', () => {
   msgpack5.encode({ a: { b: 'c' }, c: [null, null, null, { d: 'c' }] });
@@ -14,7 +20,7 @@ new Benchmark.Suite()
 .add('encode complex 1 - msgpack-lite', () => {
   msgpackLite.encode({ a: { b: 'c' }, c: [null, null, null, { d: 'c' }] });
 })
-.add('encode complex 2 - urlpack', () => {
+.add('encode complex 2 - @urlpack/msgpack', () => {
   urlpack.encode({
     'int': 1,
     'float': 0.5,
@@ -29,6 +35,25 @@ new Benchmark.Suite()
       'foo': 1,
       'baz': 0.5,
     },
+    date,
+  });
+})
+.add('encode complex 2 - @msgpack/msgpack', () => {
+  msgpack.encode({
+    'int': 1,
+    'float': 0.5,
+    'boolean': true,
+    'null': null,
+    'string': 'foo bar',
+    'array': [
+      'foo',
+      'bar',
+    ],
+    'object': {
+      'foo': 1,
+      'baz': 0.5,
+    },
+    date,
   });
 })
 .add('encode complex 2 - msgpack5', () => {
@@ -46,6 +71,7 @@ new Benchmark.Suite()
       'foo': 1,
       'baz': 0.5,
     },
+    date,
   });
 })
 .add('encode complex 2 - msgpack-lite', () => {
@@ -63,6 +89,7 @@ new Benchmark.Suite()
       'foo': 1,
       'baz': 0.5,
     },
+    date,
   });
 })
 .on('cycle', event => {
